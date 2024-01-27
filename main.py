6e3f6f713b5df321cae6406e08300e8c1ca3e26b
SaudUrRehman2899 <157876049+SaudUrRehman2899@users.noreply.github.com>
2024-01-27 00:14:37 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-# importing requests
-import requests
-# Importing json to utilize it
-import json
-# creating a variable and asking a name of city via input function
-city = input("Enter the Name of City : ")
-# creating url using my api key which i requested via login on weatherapi.com
-# adding city in url to give user more chance
-url = f"http://api.weatherapi.com/v1/current.json?key=d9b5d1a293fb4602b48115356242401&q={city}&aqi=yes"
-# creating a variable to utulize method of requests to load url
+import PyPDF2
 
-r = requests.get(url)
-# print(r.text)
-# creating a dictionary by utilizing json to differnciate between values
-wdic = json.loads(r.text)
-# Printing final output
-print(wdic["current"]["temp_c"])# Portfolio
+pdffiles= ["1.pdf","2.pdf","3.pdf"]
+
+merger = PyPDF2.PdfMerger
+
+for filename in pdffiles:
+    pdfFile = open(filename,'rb')
+    pdfReader = PyPDF2.PdfReader(pdffiles)
+    merger.append(pdfReader)
+pdfFile.close()
+merger.write('merged.pdf')
